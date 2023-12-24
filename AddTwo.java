d35b87dc7cf302b8fc52126688ec50924aa59e2b
ophereevan17 <154500072+ophereevan17@users.noreply.github.com>
2023-12-25 00:30:07 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,8 +1,11 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
-public class AddTwo {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+public  class AddTwo {
+public static void main(String[] args) {
+
+// Declares integers a, b
+
+int a = Integer.parseInt(args[0]);
+int b = Integer.parseInt(args[1]);
+
+System.out.println(a + " + " + b + " = " + (a + b));
+}
 }
